e0d4b6121d402ed4d41ea06c2a5ff8b2b7b8180c
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2021-10-05 12:29:03 +0330

    Initial settings

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    purge: [
+        'public/**/*.html',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('tailwindcss-rtl'),
+        require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
+    ],
+}
